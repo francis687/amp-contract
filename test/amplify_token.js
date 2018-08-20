1b703d8ff44f14982fe8d081507bf4370dbe6550
Substratum Developer <substratum@substratum.net>
2018-08-20 14:04:50 -0400

    AMP-68: fix quotes and global assert

--- test/amplify_token.js
+++ test/amplify_token.js
@@ -1,4 +1,4 @@
-/* global require, artifacts, contract, beforeEach, it, assert */
+/* global require, artifacts, contract, beforeEach, it */
 
 const assert = require('chai').assert
 
@@ -9,17 +9,17 @@ contract('AmplifyToken', function([owner]) {
     this.amplify_token = await AmplifyToken.new({ from: owner })
   })
 
-  it("should have the correct name", async () => {
+  it('should have the correct name', async () => {
     let name = await this.amplify_token.name()
     assert.equal(name, 'Amplify Token')
   })
 
-  it("should have the correct symbol", async () => {
+  it('should have the correct symbol', async () => {
     let symbol = await this.amplify_token.symbol()
     assert.equal(symbol, 'AMPX')
   })
 
-  it("should have the correct decimal level", async () => {
+  it('should have the correct decimal level', async () => {
     let decimals = await this.amplify_token.decimals()
     assert.equal(decimals, 18)
   })

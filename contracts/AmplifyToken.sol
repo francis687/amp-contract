41e24a2352fe7c04b6056da5f657aaab9c72f480
Substratum Developer <substratum@substratum.net>
2018-08-20 11:00:17 -0400

    AMP-68: specify decimals

--- contracts/AmplifyToken.sol
+++ contracts/AmplifyToken.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.4.22;
 contract AmplifyToken {
     string public name = "Amplify Token";
     string public symbol = "AMPX";
+    uint8 public decimals = 18;
 
     constructor() public {
     }
--- test/amplify_token.js
+++ test/amplify_token.js
@@ -5,7 +5,7 @@ const assert = require('chai').assert
 const AmplifyToken = artifacts.require('AmplifyToken')
 
 contract('AmplifyToken', function([owner]) {
-  beforeEach( async ()=> {
+  beforeEach( async () => {
     this.amplify_token = await AmplifyToken.new({ from: owner })
   })
 
@@ -17,5 +17,10 @@ contract('AmplifyToken', function([owner]) {
   it("should have the correct symbol", async () => {
     let symbol = await this.amplify_token.symbol()
     assert.equal(symbol, 'AMPX')
+  })
+
+  it("should have the correct decimal level", async () => {
+    let decimals = await this.amplify_token.decimals()
+    assert.equal(decimals, 18)
   })
 })

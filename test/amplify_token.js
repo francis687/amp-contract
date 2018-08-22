23ef52793e73ec6ff2b70febb29e3498e59094fb
Substratum Developer <substratum@substratum.net>
2018-08-22 16:53:06 -0400

    AMP-68: WIP - end of day commit - starting to time travel

--- test/amplify_token.js
+++ test/amplify_token.js
@@ -1,6 +1,5 @@
-/* global web3, require, artifacts, contract, beforeEach, it */
+/* global web3, require, artifacts, contract, before, it, describe */
 
-const assertRevert = require('./helpers/assertRevert.js')
 const BigNumber = web3.BigNumber
 const Web3 = require('web3')
 const newWeb3 = new Web3(Web3.givenProvider || 'ws://localhost:8545')
@@ -10,16 +9,24 @@ chai.use(require('chai-bignumber')(BigNumber))
 const expect = chai.expect
 const truffleAssert = require('truffle-assertions')
 
+const assertRevert = require('./helpers/assertRevert.js')
+const timeTravel = require('./helpers/timeTravel.js')
+
 const AmplifyToken = artifacts.require('AmplifyToken')
 
 const INITIAL_SUPPLY = 10 ** 27
 const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+const SECONDS_IN_A_DAY = 86400
+const SECONDS_IN_A_WEEK = 7 * SECONDS_IN_A_DAY
 
 contract('AmplifyToken', ([owner, otherAccount]) => {
   let subject
   let amplifyNewWeb3
 
-  beforeEach(async () => {
+  before(async () => {
+    let now = Math.floor(new Date().getTime() / 1000)
+    let crowdsaleEnd = now + SECONDS_IN_A_WEEK
+    console.log(crowdsaleEnd)
     subject = await AmplifyToken.new({ from: owner })
     amplifyNewWeb3 = new newWeb3.eth.Contract(subject.abi, subject.address)
   })
@@ -33,8 +40,19 @@ contract('AmplifyToken', ([owner, otherAccount]) => {
     expect(eventArgs.value).to.be.bignumber.equal(INITIAL_SUPPLY)
   })
 
+  it('cannot burn more than owner has', async () => {
+    subject.transfer(otherAccount, new BigNumber('99e25'))
+    const expectedOwnerAmount = (new BigNumber('1e25'))
+    const amountToBurn = (new BigNumber('1e26')) // more than owner has, less than totalSupply
+
+    assertRevert(subject.burn(amountToBurn))
+
+    expect(await subject.balanceOf(owner)).to.be.bignumber.equal(expectedOwnerAmount)
+    expect(await subject.totalSupply()).to.be.bignumber.equal('1e27')
+  })
+
   it('is burnable', async () => {
-    const burntAmount = new BigNumber('1e26')
+    const burntAmount = new BigNumber('1e24')
     const tx = await subject.burn(burntAmount)
 
     truffleAssert.eventEmitted(tx, 'Burn', event => {
@@ -48,22 +66,25 @@ contract('AmplifyToken', ([owner, otherAccount]) => {
         event.value.eq(burntAmount)
     })
 
-    expect(await subject.balanceOf(owner)).to.be.bignumber.equal('9e26')
-    expect(await subject.totalSupply()).to.be.bignumber.equal('9e26')
-  })
-
-  it('cannot burn more than owner has', async () => {
-    subject.transfer(otherAccount, 100)
-    const expectedOwnerAmount = (new BigNumber('1e27')).minus(100)
-    const amountToBurn = (new BigNumber('1e27')).minus(50) // more than owner has, less than totalSupply
-
-    assertRevert(subject.burn(amountToBurn))
-
-    expect(await subject.balanceOf(owner)).to.be.bignumber.equal(expectedOwnerAmount)
-    expect(await subject.totalSupply()).to.be.bignumber.equal('1e27')
+    expect(await subject.balanceOf(owner)).to.be.bignumber.equal('9e24')
+    expect(await subject.totalSupply()).to.be.bignumber.equal('999e24')
   })
 
   it('should reject receiving ETH to the fallback function', async () => {
-    assertRevert(subject.sendTransaction({ value: 1, gas: 2301 }))
+    assertRevert(subject.sendTransaction({ value: 1 }))
+  })
+
+  describe('transfers', () => {
+    it('allows transfers from the owner', async () => {
+
+    })
+
+    it('disallows transfers from non-owner before the crowdsale end', async () => {
+
+    })
+
+    it('allows transfers after the crowdsale end', async () => {
+      await timeTravel(SECONDS_IN_A_WEEK * 2)
+    })
   })
 })

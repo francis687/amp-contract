2404bb0b678e6a54c538b84aedfc84c00eb8b490
Substratum Developer <substratum@substratum.net>
2018-08-22 15:14:16 -0400

    AMP-68: add test for not accepting eth on the fallback function

--- test/amplify_token.js
+++ test/amplify_token.js
@@ -16,13 +16,16 @@ const INITIAL_SUPPLY = 10 ** 27
 const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
 contract('AmplifyToken', ([owner, otherAccount]) => {
+  let subject
+  let amplifyNewWeb3
+
   beforeEach(async () => {
-    this.amplifyToken = await AmplifyToken.new({ from: owner })
-    this.amplifyNewWeb3 = new newWeb3.eth.Contract(this.amplifyToken.abi, this.amplifyToken.address)
+    subject = await AmplifyToken.new({ from: owner })
+    amplifyNewWeb3 = new newWeb3.eth.Contract(subject.abi, subject.address)
   })
 
   it('emits an event for token creation', async () => {
-    let events = await this.amplifyNewWeb3.getPastEvents('Transfer')
+    let events = await amplifyNewWeb3.getPastEvents('Transfer')
     const eventArgs = events[0].returnValues
 
     expect(eventArgs.from.valueOf()).to.equal(ZERO_ADDRESS)
@@ -32,7 +35,7 @@ contract('AmplifyToken', ([owner, otherAccount]) => {
 
   it('is burnable', async () => {
     const burntAmount = new BigNumber('1e26')
-    const tx = await this.amplifyToken.burn(burntAmount)
+    const tx = await subject.burn(burntAmount)
 
     truffleAssert.eventEmitted(tx, 'Burn', event => {
       return event.burner === owner &&
@@ -45,18 +48,22 @@ contract('AmplifyToken', ([owner, otherAccount]) => {
         event.value.eq(burntAmount)
     })
 
-    expect(await this.amplifyToken.balanceOf(owner)).to.be.bignumber.equal('9e26')
-    expect(await this.amplifyToken.totalSupply()).to.be.bignumber.equal('9e26')
+    expect(await subject.balanceOf(owner)).to.be.bignumber.equal('9e26')
+    expect(await subject.totalSupply()).to.be.bignumber.equal('9e26')
   })
 
   it('cannot burn more than owner has', async () => {
-    this.amplifyToken.transfer(otherAccount, 100)
+    subject.transfer(otherAccount, 100)
     const expectedOwnerAmount = (new BigNumber('1e27')).minus(100)
     const amountToBurn = (new BigNumber('1e27')).minus(50) // more than owner has, less than totalSupply
 
-    assertRevert(this.amplifyToken.burn(amountToBurn))
+    assertRevert(subject.burn(amountToBurn))
 
-    expect(await this.amplifyToken.balanceOf(owner)).to.be.bignumber.equal(expectedOwnerAmount)
-    expect(await this.amplifyToken.totalSupply()).to.be.bignumber.equal('1e27')
+    expect(await subject.balanceOf(owner)).to.be.bignumber.equal(expectedOwnerAmount)
+    expect(await subject.totalSupply()).to.be.bignumber.equal('1e27')
+  })
+
+  it('should reject receiving ETH to the fallback function', async () => {
+    assertRevert(subject.sendTransaction({ value: 1, gas: 2301 }))
   })
 })

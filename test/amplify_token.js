4d6978fbdb8f55747c87ca5ad0149367edac9e93
Substratum Developer <substratum@substratum.net>
2018-08-22 13:52:32 -0400

    AMP-68: test for burn events

--- test/amplify_token.js
+++ test/amplify_token.js
@@ -7,6 +7,7 @@ const newWeb3 = new Web3(Web3.givenProvider || 'ws://localhost:8545')
 const chai = require('chai')
 chai.use(require('chai-bignumber')(BigNumber))
 const expect = chai.expect
+const truffleAssert = require('truffle-assertions')
 
 const AmplifyToken = artifacts.require('AmplifyToken')
 
@@ -16,11 +17,11 @@ const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 contract('AmplifyToken', ([owner]) => {
   beforeEach(async () => {
     this.amplifyToken = await AmplifyToken.new({ from: owner })
-    this.amplifyWeb3Contract = new newWeb3.eth.Contract(this.amplifyToken.abi, this.amplifyToken.address)
+    this.amplifyNewWeb3 = new newWeb3.eth.Contract(this.amplifyToken.abi, this.amplifyToken.address)
   })
 
   it('emits an event for token creation', async () => {
-    let events = await this.amplifyWeb3Contract.getPastEvents('Transfer')
+    let events = await this.amplifyNewWeb3.getPastEvents('Transfer')
     const eventArgs = events[0].returnValues
 
     expect(eventArgs.from.valueOf()).to.equal(ZERO_ADDRESS)
@@ -29,7 +30,19 @@ contract('AmplifyToken', ([owner]) => {
   })
 
   it('is burnable', async () => {
-    this.amplifyToken.burn(10 ** 26 + 1)
+    const burntAmount = new BigNumber('1e26')
+    const tx = await this.amplifyToken.burn(burntAmount)
+
+    truffleAssert.eventEmitted(tx, 'Burn', event => {
+      return event.burner === owner &&
+        event.value.eq(burntAmount)
+    })
+
+    truffleAssert.eventEmitted(tx, 'Transfer', event => {
+      return event.from === owner &&
+        event.to === ZERO_ADDRESS &&
+        event.value.eq(burntAmount)
+    })
 
     expect(await this.amplifyToken.balanceOf(owner)).to.be.bignumber.equal('9e26')
     expect(await this.amplifyToken.totalSupply()).to.be.bignumber.equal('9e26')

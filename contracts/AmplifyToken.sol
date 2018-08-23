83a78f4745ae2937fce6f297d2131726d4ccb521
Substratum Developer <substratum@substratum.net>
2018-08-23 13:31:28 -0400

    AMP-68: ditched solidity tests in favor of javascript; add crowdsale flag

--- contracts/AmplifyToken.sol
+++ contracts/AmplifyToken.sol
@@ -1,19 +1,25 @@
 pragma solidity ^0.4.22;
 
-import "openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/StandardBurnableToken.sol";
 
-contract AmplifyToken is StandardBurnableToken {
-    string public constant name = "Amplify Token";
+contract AmplifyToken is StandardBurnableToken, Ownable {
+    string public constant name = "AMPX";
     string public constant symbol = "AMPX";
     uint8 public constant decimals = 18;
+    bool public crowdsaleActive = true;
 
     // Number of coins (1 billion) * decimal places (18)
     uint256 public constant INITIAL_SUPPLY = (10 ** 9) * (10 ** uint256(decimals));
+
 
     constructor() public {
         totalSupply_ = INITIAL_SUPPLY;
         balances[msg.sender] = INITIAL_SUPPLY;
         emit Transfer(address(0), msg.sender, INITIAL_SUPPLY);
+    }
+
+    function endCrowdsale () public onlyOwner {
+        crowdsaleActive = false;
     }
 }
--- test/amplify_token.js
+++ test/amplify_token.js
@@ -1,4 +1,4 @@
-/* global web3, require, artifacts, contract, before, it, describe */
+/* global web3, require, artifacts, contract, before, it */
 
 const BigNumber = web3.BigNumber
 const Web3 = require('web3')
@@ -9,26 +9,41 @@ chai.use(require('chai-bignumber')(BigNumber))
 const expect = chai.expect
 const truffleAssert = require('truffle-assertions')
 
-const assertRevert = require('./helpers/assertRevert.js')
-const timeTravel = require('./helpers/timeTravel.js')
+const reverted = require('./helpers/reverted')
+const toWei = require('./helpers/toWei')
 
 const AmplifyToken = artifacts.require('AmplifyToken')
 
-const INITIAL_SUPPLY = 10 ** 27
+const INITIAL_WEI_SUPPLY = 10 ** 27
 const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
-const SECONDS_IN_A_DAY = 86400
-const SECONDS_IN_A_WEEK = 7 * SECONDS_IN_A_DAY
 
 contract('AmplifyToken', ([owner, otherAccount]) => {
   let subject
   let amplifyNewWeb3
 
   before(async () => {
-    let now = Math.floor(new Date().getTime() / 1000)
-    let crowdsaleEnd = now + SECONDS_IN_A_WEEK
-    console.log(crowdsaleEnd)
     subject = await AmplifyToken.new({ from: owner })
     amplifyNewWeb3 = new newWeb3.eth.Contract(subject.abi, subject.address)
+  })
+
+  it('has the name AMPX', async () => {
+    expect(await subject.name()).to.equal('AMPX')
+  })
+
+  it('has the symbol AMPX', async () => {
+    expect(await subject.symbol()).to.equal('AMPX')
+  })
+
+  it('has 18 decimal precision', async () => {
+    expect((await subject.decimals()).toNumber()).to.equal(18)
+  })
+
+  it('starts with a total supply of 1 billion', async () => {
+    expect(await subject.totalSupply()).to.be.bignumber.equal(toWei('1e9'))
+  })
+
+  it('starts with owner balance at 1 billion', async () => {
+    expect(await subject.balanceOf(owner)).to.be.bignumber.equal(toWei('1e9'))
   })
 
   it('emits an event for token creation', async () => {
@@ -37,7 +52,7 @@ contract('AmplifyToken', ([owner, otherAccount]) => {
 
     expect(eventArgs.from.valueOf()).to.equal(ZERO_ADDRESS)
     expect(eventArgs.to.valueOf().toLowerCase()).to.equal(owner)
-    expect(eventArgs.value).to.be.bignumber.equal(INITIAL_SUPPLY)
+    expect(eventArgs.value).to.be.bignumber.equal(INITIAL_WEI_SUPPLY)
   })
 
   it('cannot burn more than owner has', async () => {
@@ -45,7 +60,7 @@ contract('AmplifyToken', ([owner, otherAccount]) => {
     const expectedOwnerAmount = (new BigNumber('1e25'))
     const amountToBurn = (new BigNumber('1e26')) // more than owner has, less than totalSupply
 
-    assertRevert(subject.burn(amountToBurn))
+    expect(await reverted(subject.burn(amountToBurn))).to.equal(true)
 
     expect(await subject.balanceOf(owner)).to.be.bignumber.equal(expectedOwnerAmount)
     expect(await subject.totalSupply()).to.be.bignumber.equal('1e27')
@@ -71,20 +86,19 @@ contract('AmplifyToken', ([owner, otherAccount]) => {
   })
 
   it('should reject receiving ETH to the fallback function', async () => {
-    assertRevert(subject.sendTransaction({ value: 1 }))
+    expect(await reverted(subject.sendTransaction({ value: 1 }))).to.equal(true)
   })
 
-  describe('transfers', () => {
-    it('allows transfers from the owner', async () => {
+  it('starts with the crowdsale active', async () => {
+    expect(await subject.crowdsaleActive()).to.equal(true)
+  })
 
-    })
+  it('allows the owner to end the crowdsale', async () => {
+    await subject.endCrowdsale()
+    expect(await subject.crowdsaleActive()).to.equal(false)
+  })
 
-    it('disallows transfers from non-owner before the crowdsale end', async () => {
-
-    })
-
-    it('allows transfers after the crowdsale end', async () => {
-      await timeTravel(SECONDS_IN_A_WEEK * 2)
-    })
+  it('disallows non owner to end the crowdsale', async () => {
+    expect(await reverted(subject.endCrowdsale({from: otherAccount}))).to.equal(true)
   })
 })
